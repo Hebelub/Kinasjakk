bde7dcb4037eeb52f1f379a7ba3854aca45d326c
Audun Linjord Simonsen <auduns15@student.uia.no>
2020-11-21 15:56:19 +0100

    Added Board & Piece classes

---
+++ src/kinasjakk/Board.java
@@ -0,0 +1,10 @@
+package kinasjakk;
+
+public class Board {
+	Piece p1;
+
+	public Board() {
+		System.out.println("I'm a board!");
+	}
+	
+}
---
+++ src/kinasjakk/Piece.java
@@ -0,0 +1,5 @@
+package kinasjakk;
+
+public class Piece {
+
+}
