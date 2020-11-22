32a33b7a26d8b0126fae4c527611a606ddcdf500
Gabriel Løsnesløkken <glosneslokken@gmail.com>
2020-11-22 10:28:53 +0100

    Added opposite function in Direction

--- src/kinasjakk/Direction.java
+++ src/kinasjakk/Direction.java
@@ -6,5 +6,25 @@ public enum Direction {
 	TOP_LEFT,
 	TOP_RIGHT,
 	BOTTOM_LEFT,
-	BOTTOM_RIGHT
+	BOTTOM_RIGHT;
+
+	static Direction opposite (Direction d) {
+		switch (d) {
+			case LEFT:
+				return RIGHT;
+			case RIGHT:
+				return LEFT;
+			case TOP_LEFT:
+				return BOTTOM_RIGHT;
+			case BOTTOM_RIGHT:
+				return TOP_LEFT;
+			case TOP_RIGHT:
+				return BOTTOM_LEFT;
+			case BOTTOM_LEFT:
+				return TOP_RIGHT;
+			default:
+				return d;
+		}
+	}
+
 }
